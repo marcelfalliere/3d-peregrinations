b640c56cc71bfd127c01f7ea5c6c8bdc8236fcf8
William Ferreira <william.ferreira@in-tact.fr>
2014-02-09 11:23:58 +0100

    json loader sample

---
+++ jsonloader/models-json/house.js
@@ -0,0 +1,115 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "House.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 1672,
+        "faces"         : 1543,
+        "normals"       : 258,
+        "uvs"           : 2137,
+        "materials"     : 8
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Default",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "RainOut",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.287697, 0.287697, 0.287697],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "FrontFloor",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "Wall",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.57368, 0.666299, 0.8],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"mapDiffuse" : "6.JPG",
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "Door",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.57368, 0.666299, 0.8],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"mapDiffuse" : "kayu7.jpg",
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "Glass",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"specularCoef" : 256.0,
+	"transparency" : 0.3
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "Garage",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.175616, 0.175616, 0.175616],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 419610,
+	"DbgIndex" : 7,
+	"DbgName" : "Roof",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.57368, 0.666299, 0.8],
+	"colorSpecular" : [0.2, 0.2, 0.2],
+	"illumination" : 2,
+	"specularCoef" : 256.0,
+	"transparency" : 1.0
+	}],
+
+    "buffers": "house.bin"
+
+}
